843cdc4cef6268fde101c98766975bd7bb9941bb
NINA <alvarezvaninaandrea@gmail.com>
2025-03-17 01:29:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const net = require('net')
-const { v4: uuidv4 } = require('uuid')
-
-const server = net.createServer((Socke) => {
-    const idv4 = uuidv4()
-    console.log('conexion exitosa')
-    console.log('UUID V4: ', idv4)
-
-    Socke.on / ('data', (data) => {
-        console.log(`datos recibidos ${data}`)
-    })
-    Socke.on('end', () => {
-        console.log('comunicacion terminada')
+constante  neto = requerir('neto')// Importa el modulo "neto" para crear un servidor TCP .
+constante  { versión 4: uuidv4 } = requerir('uuid')// Importa la funcion uuidv4 para generar identificadores unicos .
+// Crea un servidor TCP.
+constante servidor = neto.crearServidor((Zócalo) => {
+    constante versión 4 = uuidv4() // Genera un identificador unico ( uuidv4).
+    consola.registro('conexión exitosa')//Mensaje cuando el cliene se conecta .
+    consola.registro('UUID V4: ', versión 4)//Muestra el UUID generado en la consola .
+//Maneja los datos recibidos del cliente.
+    Zócalo.en / ('data', (data) => {
+        consola.registro(`datos recibidos ${datos}`)
+//Maneja la finalizacion de la conversacion .        
+Zócalo.en('fin', () => {
+        consola.registro('comunicación terminada')
     })
 })
-
-server.listen(8080, () => {
-    console.log('servidor escuchando en el puerto 8080')
+//Hace que el servidor escuche en el puerto 8080.
+servidor.escuchar(8080, () => {
+    consola.registro('servidor escuchando en el puerto 8080')
 }
 )
 
