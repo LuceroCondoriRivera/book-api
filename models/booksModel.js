eb0d41531ceb20bb7bc82f76fb8a10d7bf67c2f5
NINA <alvarezvaninaandrea@gmail.com>
2025-03-18 19:46:54 -0300

    Update booksModel.js

--- models/booksModel.js
+++ models/booksModel.js
@@ -1,16 +1,19 @@
-//Importamos el modulo fs de Node.js para realizar operaciones de archivos
+//Importamos el modulo fs de Node.js para realizar operaciones de archivos.
 
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join(__dirname, './books.json')
-const getBookById = (id) => {
-    try {
-        const data = fs.readFileSync(filePath, 'utf-8')
-        const books = JSON.parse(data)
-        return books.find(book => book.id === id) || null
-    } catch (error) {
-        console.error('error al leer el archivo json: ', error)
-        return null
+constante fs = requerir('fs')
+//Importamos el módulo 'path'para manejar rutas de archivos de forma segura .
+constante camino = requerir camino('camino')
+//Definimos la ruta del ejercicio JSON donde se almacenan los libros.
+constante ruta de archivo = camino.unirse(__nombredirectorio, './libros.json')
+
+constante obtenerLibroPorId = (identificación) => {
+    intentar {
+        constante datos = fs.readFileSync(ruta de archivo, 'utf-8') //Leemos el contenido del archivo books.json de forma síncrona.
+        constante libros = JSON.analizar gramaticalmente(datos) // Parseamos el contenido JSON para convertirlo en un array de objetos .
+        devolver libros.encontrar(libro => libro.identificación === identificación) || nulo  //Buscamos el libro cuyo ID coincida con el proporcionado.
+    } atrapar (error) {
+        consola.error('error al leer el archivo json: ', error) // Capturamos cualquier error al leer el archivo ao a parsear el JSON.
+        devolver nulo
     }
 }
-module.exports = getBookById
+módulo.exportaciones = obtenerLibroPorId // Exportamos la función para que pueda ser utilizada en otros módulos.
