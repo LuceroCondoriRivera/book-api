c4290547843ccb5a534e7c662d19124ad2757b08
NINA <alvarezvaninaandrea@gmail.com>
2025-03-18 22:16:00 -0300

    Update authorControllers.js

--- controllers/authorControllers.js
+++ controllers/authorControllers.js
@@ -1,9 +1,8 @@
-const { findAuthor } = require('../models/authorsModel');
+const { findAuthor } = require('../models/authorsModel'); // Permite buscar un autor por su nombre en la base de datos .
 
 
-const handleAuthorsRequest = (authorName) => {
-
-    return findAuthor(authorName);
+const handleAuthorsRequest = (authorName) => { // Maneja la solicitud para buscar un autor por su nombre .
+    return findAuthor(authorName);// Llama a la función y retorna el resultado.
 };
 
-module.exports = { handleAuthorsRequest };
+module.exports = { handleAuthorsRequest };// Exporta la función para que pueda ser utilizada en otros archivos del proyecto.
