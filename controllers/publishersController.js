9987a5be2ecfcdeb2af7cbee2f0c7e773a078524
NINA <alvarezvaninaandrea@gmail.com>
2025-03-18 21:55:43 -0300

    Update publishersController.js

--- controllers/publishersController.js
+++ controllers/publishersController.js
@@ -1,8 +1,8 @@
-const { getPublishers } = require('../models/publishersModel');
+const { getPublishers } = require('../models/publishersModel'); // Permite acceder a la lista de editoriales almacenadas en el archivo JSON.
 
-const handlePublishersRequest = () => {
+const handlePublishersRequest = () => { //Define una función para poder obtener la lista de editoriales.
 
-    return getPublishers();
+    return getPublishers(); // Llama a la funcion 'getPublisher' para obtener la lista de editoriales y devuelve el resultado.
 };
 
-module.exports = { handlePublishersRequest };
+module.exports = { handlePublishersRequest };// Exporta la función para que pueda ser utilizadas en otros archivos del proyectos .
